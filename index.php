f7b1cdbbcc23eb720aa5609ca55d9977d3a338c2
ubanitc <57474291+ubanitc@users.noreply.github.com>
2020-05-02 08:01:16 -0700

    Update index.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ if(isset($_SESSION['userid'])){
             ?>
          <h5>Welcome <?php echo $user->dpname ?></h5>
         </div>
-                <?php if( $user->acctno === null or $user->bank === null) { ?>
+                <?php if( $user->acctno === 'not set' or $user->bank === 'not set') { ?>
                 
                     <div class="alert alert-warning alert-dismissible fade show" role="alert">
   <strong>Update Account Details</strong> Your account details are not set <a href="https://trencynews.herokuapp.com/updateaccount.php"><button class="btn btn-success" style="float:right;">Update Account</button></a>
