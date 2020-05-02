7649b46993e182cfd31833069f60950b34188bee
ubanitc <57474291+ubanitc@users.noreply.github.com>
2020-05-02 04:32:16 -0700

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -13,7 +13,7 @@ if(isset($_SESSION['userid'])){
         }
 
     }else{
-        header('location:login.php');
+        header('location:http://trencynews.herokuapp.com/login.php');
     }
 
 ?>
@@ -76,7 +76,7 @@ if(isset($_SESSION['userid'])){
 
         <div class="card-body">
                 <h4>Affiliate Link:</h4>
-                <p>https://www.trencynews.com/register.php?refid=<?php echo $user->name?></p>
+                <p>http://trencynews.herokuapp.com/register?refid=<?php echo $user->name?></p>
         </div>
 </div>
 
