fdcf5ea7870eaec7e32ea86290ae04443c6ac8b5
ubanitc <57474291+ubanitc@users.noreply.github.com>
2020-05-03 09:52:14 -0700

    Update index.php

--- index.php
+++ index.php
@@ -81,7 +81,7 @@ if(isset($_SESSION['userid'])){
       </div>
     </div>
 <?php
-    
+    <?php
             $stmt = $pdo->query("SELECT * FROM posts ORDER BY id DESC");
             $posts = $stmt->fetchAll(); ?>
             <div class="container">
@@ -99,15 +99,5 @@ if(isset($_SESSION['userid'])){
 <?php require("./inc/footer.html");?>
 
 
-
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-    crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-    crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-    crossorigin="anonymous"></script>
-    <script src="show-hide-password.js"></script>
-    <script>
+<script src="./bootstrap-4.4.1-dist/js/bootstrap.min.js"></script>
+  <script src="./bootstrap-4.4.1-dist/js/bootstrap.bundle.min.js"></script>
